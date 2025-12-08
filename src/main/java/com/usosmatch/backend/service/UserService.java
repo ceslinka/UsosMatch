4dad94332bd9d5b900566cac793d8aa8aec57b40
rozalia <rozaliam@example.com>
2025-12-08 13:02:44 +0100

    Dodałam UserRepository i UserService

---
+++ src/main/java/com/usosmatch/backend/service/UserService.java
@@ -0,0 +1,27 @@
+package com.usosmatch.backend.service;
+
+import java.util.List;
+import com.usosmatch.backend.model.User;
+import org.springframework.stereotype.Service;
+import com.usosmatch.backend.UserRepository;
+
+import java.util.List;
+
+@Service // Uzywamy Springa, zamiast pisać "UserService service = new UserService(repo);'
+
+public class UserService {
+    private final UserRepository userRepository; // final oznacza że pole musi zostać wypelnione raz i nie moze się zmienic
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    } // wypelniamy userRepository w momencie tworzenia obiektu UserService
+    public User registerUser(User user) {
+        System.out.println("Rejestruję nowego użytkownika: " + user.getEmail());
+        return userRepository.save(user); // to metoda która robi INSERT w bazie
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}

b908b914cc5b3434ed8475c857163630dbc41420
rozalia <rozaliam@example.com>
2025-12-27 11:45:53 +0100

    Houston mamy Kontroler

---
+++ src/main/java/com/usosmatch/backend/controller/UsosMatchController.java
@@ -0,0 +1,21 @@
+package com.usosmatch.backend.controller;
+import com.usosmatch.backend.model.User;
+import com.usosmatch.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/api")
+public class UsosMatchController {
+    private final UserService userService;
+    public UsosMatchController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+}

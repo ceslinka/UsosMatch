bc8b9738c72993d976d87be9e333541397dc1900
kPawelcz <kpawelczak@student.agh.edu.pl>
2025-12-29 18:45:35 +0100

    Dokonczenie Glownego controllera

--- src/main/java/com/usosmatch/backend/controller/UsosMatchController.java
+++ src/main/java/com/usosmatch/backend/controller/UsosMatchController.java
@@ -1,21 +1,37 @@
 package com.usosmatch.backend.controller;
+import com.usosmatch.backend.model.Match;
 import com.usosmatch.backend.model.User;
+import com.usosmatch.backend.service.MatchingService;
 import com.usosmatch.backend.service.UserService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
 @RestController // Zwracamy JSON
 @RequestMapping("/api")
 public class UsosMatchController {
     private final UserService userService;
-    public UsosMatchController(UserService userService) {
+    private final MatchingService matchingService;
+
+    public UsosMatchController(UserService userService, MatchingService matchingService) {
         this.userService = userService;
+        this.matchingService = matchingService;
     }
+
+
     @PostMapping("/users")
     public User createUser(@RequestBody User user) { // RequestBody zmienia z JSON na obiekt JAVA
         return userService.registerUser(user);
     }
+
     @GetMapping("/users")
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
+
+    @GetMapping("/matches/{userId}")
+    public List<Match> getUserMatches(@PathVariable Long userId){
+        List<Match> matches = matchingService.generateMatches(userId);
+        return matches;
+    }
+
 }
